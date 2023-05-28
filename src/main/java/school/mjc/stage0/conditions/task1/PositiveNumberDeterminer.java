9ba90f191a06deaf06a16a1f60be8b62a1488905
JustDevWeb <zm91672@gmail.com>
2023-05-28 12:29:35 +0800

    dev: module4: task1 done

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+        if(0 < numberToBeDetermined) {
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
     }
 }
